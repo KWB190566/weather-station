ce063d13b080cfb7247a42e5a2af38625cc27ebf
Sascha <sascha.hofmann@gyana.co.uk>
2020-04-05 11:56:48 +0100

    Initial commit

---
+++ download.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import os
+from datetime import datetime as dt
+import pandas as pd
+from dotenv import load_dotenv
+import json
+load_dotenv()
+
+def request_with_retry(url, params={}, headers={}):
+    '''Retry a request at 1s time interval.'''
+
+    try:
+        with requests.get(url, params=params, headers=headers) as page:
+            return page
+    except: # explicit errors ?
+        raise
+
+def convert_to_datetime(text):
+    return dt.strptime(text,'%d.%m.%Y %H:%M:%S')
+def convert_temp(text):
+    return text.replace(',', '.').replace(' C','')
+def convert_humidity(text):
+    return text.replace(',', '.').replace('%','')
+def get_rows(page):
+    rows = page.find('table', {'class': 'table table-striped'})
+    if rows ==None:
+        return
+    rows = rows.find_all('tr')
+    return rows
+def get_page(url, params):
+    page = request_with_retry(url = url, params = params)
+    return BeautifulSoup(page.text, 'html.parser')
+def transform_cols(df):
+    df.Zeitpunkt = df.Zeitpunkt.apply(convert_to_datetime)
+    df['Temperatur Innen'] = df['Temperatur Innen'].apply(convert_temp)
+    df['Temperatur Außen'] =  df['Temperatur Außen'].apply(convert_temp)
+    df['Luftfeuchte Innen'] =  df['Luftfeuchte Innen'].apply(convert_humidity)
+    df['Luftfeuchte Außen'] =  df['Luftfeuchte Außen'].apply(convert_humidity)
+    df = df.set_index('Zeitpunkt')
+    return df
+
+
+url = 'https://measurements.mobile-alerts.eu/Home/MeasurementDetails?'
+
+device_info = json.loads(os.getenv('STATION_ID'))
+params = {**device_info,
+    'appbundle': 'eu.mobile_alerts.mobilealerts',
+    'pagesize': 250,
+    'page': 1
+}
+
+
+lines = []
+for i in range(50):
+    params['page'] = i +1
+    print('Page:', params['page'])
+    page = get_page(url, params)
+    rows = get_rows(page)
+    if rows ==None:
+        print(f'Exceeded request limit after {i} requests.')
+        break
+    if i==0:
+        header = [i.text for i in rows[0].find_all('th')]
+    for row in rows[1:]:
+        row = [i.text for i in row.find_all('td')]
+        lines.append(row)
+print('Alle Seite gelesen.')
+df = pd.DataFrame(lines, columns=header)
+
+df = transform_cols(df)
+now = dt.today()
+filename = f'wetterstation_{now.day}-{now.month}-{now.year}.csv'    
+df.to_csv(filename)
